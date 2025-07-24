c85f40a1f5cd6201e9536921ea1a03862d9f4bab
Shoaib Ali Asif <eagleshebi@gmail.com>
2025-07-24 10:27:40 +0500

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,24 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  task="";
+  taskList:{id:number,task:string}[]=[]
+
+addTask(){
+  this.taskList.push({id:this.taskList.length+1,task:this.task})
+  this.task=""
+}
+deleteTask(taskId:number){
+  this.taskList=this.taskList.filter((item)=>item.id!=taskId)
+
+
+}
+}
